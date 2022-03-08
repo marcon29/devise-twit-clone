817663e22d49827abade3f6e2994378487e55487
marcon29 <online@markstabler.com>
2022-03-07 19:52:48 -0700

    associated Post and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. 
-  devise  :database_authenticatable, 
-          :registerable,
-          :recoverable, 
-          :rememberable, 
-          :validatable
-  # Others available are:
-          # :confirmable, 
-          # :lockable, 
-          # :timeoutable, 
-          # :trackable,
-          # :omniauthable
+        # Default devise modules:
+        devise  :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+        # Others available are:
+        # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
+
+        has_many :posts
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_220710) do
+ActiveRecord::Schema.define(version: 2022_03_08_024318) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
